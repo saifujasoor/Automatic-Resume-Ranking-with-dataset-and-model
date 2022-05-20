91c76be62dff9c77b6755216996ccbb27316ddef
saifujasoor <codewithsaifullah@gmail.com>
2022-05-20 19:01:09 +0530

    Resume Ranking

--- Extract/zipextract.py
+++ Extract/zipextract.py
@@ -8,7 +8,7 @@ logging.basicConfig(
 li = os.listdir(
     '/home/saifullah/Desktop/Automatic-Resume-QA/Model/stackexchange')
 
-comm = "7z x -y stackexchange/"
+comm = "7z x -y stackexchange/"   #format and folder name
 
 for i in range(1, len(li)):
     name = li[i]
@@ -29,7 +29,7 @@ import glob
 from pyunpack import Archive
 
 for i, f in enumerate(glob.glob(os.path.join("/home/saifullah/Desktop/Automatic-Resume-QA/Model/stackexchange", "*.7z"))):
-    dir_path = "/home/saifullah/Desktop/Automatic-Resume-QA/stackexchange/dataset" + \
+    dir_path = "/home/saifullah/Desktop/Automatic-Resume-QA/stackexchange/dataset" + \  		#destination path
         str(i+1)
     os.mkdir(dir_path)
     Archive(f).extractall(dir_path)
